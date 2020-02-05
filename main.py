db122dc9555ee63e8df89fd83769dbe8d751c682
Emre <emrecanpinar06@gmail.com>
2020-02-05 23:04:03 +0300

    dronekit functions first version added

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+from dronekit import connect, Command, LocationGlobal,VehicleMode
+from pymavlink import mavutil
+import time, sys, argparse, math
+import threading
+"""
+Not needed for now
+
+from statistics import mean
+import cv2
+import numpy as np
+import picamera
+"""
+#################################### Connection String and vehicle connection
+"""
+connection_string       = '127.0.0.1:14540'
+
+print ("Connecting")
+vehicle = connect(connection_string, wait_ready=True)
+"""
+connection_string       = "/dev/ttyAMA0"
+
+print ("Connecting")
+vehicle = connect(connection_string,baud=57600, wait_ready=True)
+
+
+##################################### Objects
 class LandSiteObject:
     def __init__(self,lat,lon,flagName,letter):
         self.lat = lat
@@ -11,26 +37,8 @@ class FlagObject:
         self.landOrder = landOrder
         self.flagName = flagName
 
-def TakeOff(desitedAltitude):
-    #takeoff to desired altitude
-    return True
 
-def GoToLocation(lat,lon,alt):
-    #goto desired locaiton
-    return True
-
-def DefineTheFlag(landSiteLetter):
-    #define the flag and write name of it to the landSite
-    return True 
-
-def LandWithVision(flagName):
-    #land with vision to the flag
-    return True
-
-def RTL():
-    #return to launch
-    return True
-
+##################################### SETUP
 STM = FlagObject('N',1,"STM")
 ODTU = FlagObject('N',2,"ODTU")
 ORT = FlagObject('N',3,"ORT")
@@ -44,15 +52,136 @@ D = LandSiteObject(2,-2,"N",'D')
 land_sites = [A,B,C,D]
 flag_objects = [STM,ODTU,ORT,LAND]
 
-TakeOff(8) #Takeoff to 8 meters
 
-#LOOP:
+x,y,z =0,0,0
+
+vision_altitude = 6 #meter
+
+drive_with_meter = 0b111111111000
+drive_with_speed = 0b111111000111
+
+drive_type = drive_with_meter #initial
+
+###################################### FUNCTIONS
+
+def setpoint_buffer():
+    global x,y,z,drive_type
+    while True:
+        msg=vehicle.message_factory.set_position_target_local_ned_encode(
+            0,
+            0,0,
+            mavutil.mavlink.MAV_FRAME_LOCAL_NED,
+            drive_type,
+            x,y,z,
+            x,y,z,
+            0,0,0,
+            0,0)
+        vehicle.send_mavlink(msg)
+        vehicle.flush()
+        time.sleep(0.3)
+
+def land():
+    print("Land !")
+    while vehicle.mode != "LAND":
+        vehicle._master.set_mode_px4('LAND',None,None)
+        print ("Trying land")
+        time.sleep(0.3)
+    time.sleep(2)
+    print ("Landed!")
+
+def tryArming():
+    while True:
+        vehicle._master.mav.command_long_send(
+        1, # autopilot system id
+        1, # autopilot component id
+        400, # command id, ARM/DISARM
+        0, # confirmation
+        1, # arm!
+        0,0,0,0,0,0 # unused parameters for this command
+        )
+        print("Trying arming")
+        time.sleep(0.3)
+        if (vehicle.armed == True):
+            break
+    print ("Armed :",vehicle.armed)
+
+
+def startThread():
+    t = threading.Thread(target=setpoint_buffer)
+
+    t.daemon = True
+
+    t.start()
+
+def startOffboardMode():
+    while vehicle.mode != "OFFBOARD":
+        vehicle._master.set_mode_px4('OFFBOARD',None,None)
+        print ("Trying offboard")
+        time.sleep(0.3)
+
+def print_status():
+    # Display basic vehicle state
+    print (" Type: %s" % vehicle._vehicle_type)
+    print (" Armed: %s" % vehicle.armed)
+    print (" System status: %s" % vehicle.system_status.state)
+    print (" GPS: %s" % vehicle.gps_0)
+    print (" Alt: %s" % vehicle.location.global_relative_frame.alt)
+
+
+
+def takeOff(desitedAltitude):
+    #takeoff to desired altitude
+    return True
+
+def goToLocation(lat,lon,alt):
+    #goto desired locaiton
+    return True
+
+def defineTheFlag(landSiteLetter):
+    #define the flag and write name of it to the landSite
+    return True 
+
+def landWithVision(flagName):
+    #land with vision to the flag
+    return True
+
+def rtl():
+    #return to launch
+    return True
+
+
+
+
+
+##################################### START
+home_position_set = False
+#Create a message listener for home position fix
+@vehicle.on_message('HOME_POSITION')
+def listener(self, name, home_position):
+    global home_position_set
+    home_position_set = True
+
+
+while not home_position_set:
+    print ("Waiting for home position...")
+    time.sleep(1)
+
+print_status()
+
+tryArming()
+startThread()
+startOffboardMode()
+
+
+takeOff(8) #Takeoff to 8 meters
 i = 1
+###################################### LOOP
+
 while True:
     print("Land Order = ",i)
 
     if (i == 5):#So we should RTL
-        RTL()
+        rtl()
         break
 
     for flag in flag_objects:
@@ -60,13 +189,16 @@ while True:
             if(flag.landSiteLetter == 'N'):
                 for land in land_sites:
                     if (land.flagName == 'N'):
-                        GoToLocation(land.lat,land.lon,8)
-                        DefineTheFlag(land.letter)
+                        goToLocation(land.lat,land.lon,vision_altitude)
+                        defineTheFlag(land.letter)
             else:
                 for land in land_sites:
                     if (flag.landSiteLetter == land.letter):
-                        GoToLocation(land.lat,land.lon,8)
-                        LandWithVision(flag.flagName)
+                        goToLocation(land.lat,land.lon,vision_altitude)
+                        landWithVision(flag.flagName)
                         #sleep for some time
-                        TakeOff(8)
+                        takeOff(vision_altitude)
                         i += 1
+
+
+vehicle.close()
